3c313b346614f10a267083aa5ab55d92e4bc6dc2
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2022-11-14 21:12:02 +0200

    create extension

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -2,9 +2,15 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 import 'package:tut_app/app/constant.dart';
+import 'package:tut_app/data/response/responses.dart';
 part 'app_api.g.dart';
 
 @RestApi(baseUrl: Constant.baseurl)
 abstract class  AppServicesClient{
   factory AppServicesClient(Dio dio,{String baseUrl}) =_AppServicesClient;
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+      @Field("email") String email,
+      @Field("password") String password,
+      );
 }
--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -13,3 +13,22 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.numOfSlides, this.currentIndex);
 }
+class Authentication{
+  Customer customer;
+  Contact contact;
+  Authentication(this.customer,this.contact,);
+}
+// login models
+class Customer{
+  String id;
+  String name;
+  int numOfNotifications;
+  Customer(this.id,this.name,this.numOfNotifications,);
+}
+
+class Contact{
+  String phone;
+  String email;
+  String link;
+  Contact(this.phone,this.email,this.link,);
+}
